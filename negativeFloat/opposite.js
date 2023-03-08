374859e9732c31212e12d20a6ff05e378d9d304b
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2023-03-08 10:13:45 +0700

    opposite

---
+++ negativeFloat/opposite.js
@@ -0,0 +1,19 @@
+// function opposite(number) {
+//   //your code here
+//   if (number < 0) {
+//     return parseFloat(String(number).slice(1))
+//   } else if (number > 0) {
+//     return -number
+//   } else {
+//     return 0
+//   }
+// }
+
+// console.log(opposite(-0.56))
+
+//Gak kepikiran sumpah buat dikali wkwkwkwk
+function opposite(number) {
+  return number * (-1)
+}
+
+console.log(opposite(-8776.98787))
